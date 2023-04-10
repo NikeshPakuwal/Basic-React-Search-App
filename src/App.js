2b4bda3d8a0de722644539ff6fc76281fc5c40cc
NikeshPakuwal <pakuwalnikesh@gmail.com>
2023-04-10 18:18:09 +0545

    rendering list of images

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { useState } from 'react';
 import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
 import searchImages from './api';
 
 
 function App(){
 
+    const [images, setImages] = useState([]);
+
     const handleSubmit = async (term) =>{
-        await searchImages(term);
+        const result = await searchImages(term);
+        setImages(result);
     };
     
-    return <div>
-        <SearchBar onSubmit={handleSubmit} />
-    </div>
+    return (
+        <div>
+            <SearchBar onSubmit={handleSubmit} />
+            <ImageList images={images} />
+        </div>
+    );
 }
 
 export default App;
